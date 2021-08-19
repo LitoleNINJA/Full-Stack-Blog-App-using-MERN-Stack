5034547e6dba95322ec2abef16de21981502897d
LitoleNINJA <sritwik2@gmail.com>
2021-08-19 18:40:42 +0530

    prepared for deployment

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static('client/build'));
 app.use("/images", express.static(path.join(__dirname, "/images")));
 dotenv.config();
 
@@ -38,13 +39,10 @@ app.post('/upload', upload.single('file'), (req, res) => {
 });
 
 app.use('/posts', postRoute);
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+    
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log("RUNNING FINE");
